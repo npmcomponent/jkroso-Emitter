8df8683b339c2e96e54eb87f0a98dbedcddc1625
Jakeb Rosoman <jkroso@gmail.com>
2012-10-22 01:03:12 +1300

    added files

---
+++ dist/Emitter.js
@@ -0,0 +1,98 @@
+// Emitter - v0.5.0 - 2012-10-22
+// https://github.com/jkroso/Emitter
+// Copyright (c) 2012 Jakeb Rosoman; Licensed MIT
+
+
+define('Emitter',[],function () { 
+    
+    function Emitter (obj) {
+        if ( obj ) {
+            Object.keys(Emitter.prototype).forEach(function (key) {
+                if ( ! obj.hasOwnProperty(key) ) {
+                    Object.defineProperty(obj, key, { 
+                        value: Emitter.prototype[key], 
+                        writable:true,
+                        configurable:true 
+                    })
+                }
+            })
+            return Emitter.call(obj)
+        }
+        // Using discriptor to prevent non-subTopic properties from being enumerable
+        Object.defineProperties(this, {
+            _callbacks : {
+                value : {},
+                writable : true
+            }
+        })
+    }
+    
+    var proto = Emitter.prototype
+    
+    proto.publish = function (topic, data) {
+        var calls
+        if ( calls = this._callbacks ) {
+            calls = calls[topic]
+            if (calls) {
+                var i = calls.length
+                while (i--) {
+                    calls[i].call(calls[--i], data)
+                }
+            }
+        }
+        return this
+    }
+    
+    proto.on = function (topics, callback, context) {
+        var calls = this._callbacks || (this._callbacks = {})
+        topics.split(/\s+/).forEach(function (topic) {
+            calls[topic] = [context || this, callback].concat(calls[topic] || [])
+        }, this)
+        return this
+    }
+    
+    proto.once = function (topics, callback, context) {
+        topics.split(/\s+/).forEach(function (topic) {
+            var self = this
+            function on (data) {
+                self.off(topic, on)
+                callback.call(context, data)
+            }
+            this.on(topic, callback, context)
+        })
+        return this
+    }
+    
+    proto.off = function (topics, callback) {
+        var calls = this._callbacks
+        if ( calls ) {
+            if ( topics ) {
+                if ( callback ) {
+                    topics.split(/\s+/).forEach(function (topic) {
+                        var events = calls[topic]
+                        if ( events ) {
+                            events = events.slice()
+                            var i = events.length
+                            while (i--) {
+                                if (events[i--] === callback) {
+                                    events.splice(i, 2)
+                                    calls[topic] = events
+                                    break
+                                }
+                            }
+                        }                
+                    })
+                } else {
+                    topics.split(/\s+/).forEach(function (topic) {
+                        delete calls[topic]
+                    })
+                }
+            } else {
+                this._callbacks = {}
+            }
+        }
+        return this
+    }
+
+    return Emitter
+});
