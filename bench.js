63c010e4f7c822a905a279efd1e33f7021ccbae2
jkroso <jkroso@gmail.com>
2013-05-06 16:14:02 +1200

    benchmark light.js

--- bench.js
+++ bench.js
@@ -1,15 +1,26 @@
 
 var b = require('b')
   , Emitter = require('./')
+  , Light = require('./light')
 
-function bench(name){
-	return b(name).reporter('json')
+function bench(weight, i){
+	var args = ['\'run\'']
+	var c = 0
+	while (c++ < i) args.push('i')
+	var ƒ = eval('(function(i){'+weight+'.emit('+args.join()+')})')
+	b(weight+': argument.length = '+i)
+		.reporter('json')
+		.run(iters, ƒ)
 }
 
 var emitter = new Emitter
+var light = new Light
 var iters = 1e5
 
 emitter.on('run', function(a,b,c,d){
+	// arguments[arguments.length -1]()
+})
+light.on('run', function(a,b,c,d){
 	// arguments[arguments.length -1]()
 })
 
@@ -19,32 +30,11 @@ function write(t){
 
 write('[')
 
-bench('arguments.length: 0').run(iters, function (i) {
-	emitter.emit('run')
-})
-
-write(',')
-
-bench('arguments.length: 1').run(iters, function (i) {
-	emitter.emit('run', i)
-})
-
-write(',')
-
-bench('arguments.length: 2').run(iters, function (i) {
-	emitter.emit('run', i, i)
-})
-
-write(',')
-
-bench('arguments.length: 3').run(iters, function (i) {
-	emitter.emit('run', i, i, i)
-})
-
-write(',')
-
-bench('arguments.length: 4').run(iters, function (i) {
-	emitter.emit('run', i, i, i, i)
-})
+for (var i = 0; i < 5; i++) {
+	bench('emitter', i)
+	write(',')
+	bench('light', i)
+	if (i != 4) write(',')
+}
 
 write(']')
