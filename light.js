0ed134e0f85bee5014bf75dfb465287adbe9fb6f
jkroso <jkroso@gmail.com>
2013-05-06 15:56:01 +1200

    add light implementation
    by "light" I mean with mechanical sympathy

---
+++ light.js
@@ -0,0 +1,139 @@
+/**
+ * A highly optimised emitter implementation. Optimised to 
+ * minimize both memory and CPU consumption. Its good for 
+ * implementing simple but hot things like streams. 
+ */
+
+var call = Function.prototype.call
+
+module.exports = Emitter
+
+/**
+ * Generate a new Emitter or mixin methods to `obj`
+ *
+ *   var emitter = new Emitter
+ *   var emitter = Emitter({})
+ */
+
+function Emitter (obj) {
+	if (obj) {
+		for (var prop in proto) {
+			obj[prop] = proto[prop]
+		}
+		return obj
+	}
+	this._events = {}
+}
+
+var proto = Emitter.prototype
+
+/**
+ * Generate an event. All arguments after `topic` will be passed to
+ * the handlers
+ *
+ *   emitter.emit('event', new Date)
+ *   
+ * @param {String} topic the events topic
+ * @param {Any} [...]
+ */
+
+Emitter.prototype.emit = function (topic) {
+	var sub = this._events
+	if (!(sub && (sub = sub[topic]))) return
+	// single subsription case
+	if (typeof sub == 'function') {
+		// avoid using .apply() for speed
+		switch (arguments.length) {
+			case 1: sub.call(this);break
+			case 2: sub.call(this, arguments[1]);break
+			case 3: sub.call(this, arguments[1], arguments[2]);break
+			case 4: sub.call(this, arguments[1], arguments[2], arguments[3]);break
+			default:
+				// `arguments` is magic :)
+				topic = this
+				call.apply(sub, arguments)
+		}
+	} else {
+		var i = 0
+		var ƒ
+		switch (arguments.length) {
+			case 1: while (ƒ = sub[i++]) ƒ.call(this);break
+			case 2: while (ƒ = sub[i++]) ƒ.call(this, arguments[1]);break
+			case 3: while (ƒ = sub[i++]) ƒ.call(this, arguments[1], arguments[2]);break
+			case 4: while (ƒ = sub[i++]) ƒ.call(this, arguments[1], arguments[2], arguments[3]);break
+			default:
+				topic = this
+				while (ƒ = sub[i++]) call.apply(ƒ, arguments)
+		}
+	}
+}
+
+/**
+ * subscribe `fn` to `topic`
+ *
+ *   emitter.on('event', function(data){})
+ *
+ * @param {String} topic
+ * @param {Function} fn
+ * @return {fn}
+ */
+
+Emitter.prototype.on = function (topic, fn) {
+	var events = this._events || (this._events = {})
+	var subs = events[topic] 
+	if (!subs) events[topic] = fn
+	else if (typeof subs == 'function') {
+		events[topic] = [events[topic], fn]
+	} else {
+		events[topic] = subs.concat(fn)
+	}
+	return fn
+}
+
+/**
+ * Remove subscriptions
+ *
+ *   emitter.off() // clears all topics
+ *   emitter.off('topic') // clears all handlers from the topic 'topic'
+ *   emitter.off('topic', fn) // as above but only if the handler === fn
+ *
+ * @param {String} [topic]
+ * @param {Function} [fn]
+ */
+
+Emitter.prototype.off = function (topic, fn) {
+	var events = this._events
+	if (!events) return
+
+	// no filters
+	if (topic == null) {
+		for (var i in events) delete events[i]
+	} 
+	// just a topic
+	else if (!fn) {
+		delete events[topic]
+	}
+	else {
+		var subs = events[topic]
+		if (!subs) return
+		if (typeof subs == 'function') {
+			if (subs === fn) delete events[topic]
+		} else {
+			subs = events[topic] = subs.filter(function(ƒ){
+				return ƒ !== fn
+			})
+			// tidy
+			if (subs.length == 1) events[topic] = subs[0]
+			else if (!subs.length) delete events[topic]
+		}
+	}
+}
+
+Emitter.prototype.once = function (topic, fn) {
+	if (!fn) return
+	var self = this
+	this.on(topic, function once() {
+		fn.apply(this, arguments)
+		self.off(topic, once)
+	})
+}
---
+++ test/light.test.js
@@ -0,0 +1,203 @@
+
+var Emitter = require('../light')
+	, should = require('chai').should()
+
+function noopA () {}
+function noopB () {}
+
+/**
+ * helper to assert the existence of a subscription
+ */
+
+function hasSubscription (topic, fn) {
+	var calls = emitter._events[topic]
+	if (typeof calls == 'function') {
+		if (calls !== fn) throw new Error('wrong subscription')
+	} else if (calls instanceof Array) {
+		if (calls.indexOf(fn) < 0) error()
+	} else {
+		error()
+	}
+
+	function error(){
+		throw new Error('Subscription not found in '+topic)
+	}
+}
+
+/**
+ * helper to assert a subscription doesn't exist
+ */
+
+function notSubscription (topic, fn) {
+	var calls = emitter._events[topic]
+	if (typeof calls == 'function') {
+		if (calls === fn) error()
+	} else if (calls instanceof Array) {
+		if (calls.indexOf(fn) >= 0) error()
+	}
+
+	function error(){
+		throw new Error('subcription exists')
+	}
+}
+
+var emitter
+
+describe('light.js', function () {
+	beforeEach(function () {
+		emitter = new Emitter
+	})
+
+	describe('sub-classing', function(){
+		it('with Emitter.call(this)', function(done){
+			function Custom() {
+				Emitter.call(this)
+			}
+			Custom.prototype.__proto__ = Emitter.prototype
+			var emitter = new Custom
+			emitter.on('foo', done)
+			emitter.emit('foo')
+		})
+
+		it('without Emitter.call(this)', function (done) {
+			function Custom(){}
+			Custom.prototype.__proto__ = Emitter.prototype
+			var emitter = new Custom
+			emitter.on('foo', done)
+			emitter.emit('foo')
+		})
+	})
+
+	describe('Mixin', function () {
+		it('should return the target object', function () {
+			var o = {}
+			Emitter(o).should.equal(o)
+		})
+
+		it('should look like an Emitter instance', function () {
+			Emitter({}).should.have.keys([
+				'emit',
+				'on',
+				'off',
+				'once'
+			])
+		})
+	})
+
+	describe('.on(String, Function)', function () {
+		it('should register the callback', function () {
+			emitter.on('test', noopA)
+			hasSubscription('test', noopA)
+		})
+
+		it('should be able to subscribe multiple functions per event', function () {
+			emitter.on('test', noopA)
+			emitter.on('test', noopB)
+			hasSubscription('test', noopA)
+			hasSubscription('test', noopB)
+		})
+
+		it('should return the function which was subscribed', function () {
+			emitter.on('test', noopA).should.equal(noopA)
+		})
+	})
+
+	describe('.emit(String, ...)', function () {
+		it('Should fire in the order functions were subscribed', function () {
+			var c = 0
+
+			emitter.on('a', function () {
+				(++c).should.equal(1)
+			})
+			
+			emitter.on('a', function () {
+				(++c).should.equal(2)
+			})
+			
+			emitter.emit('a')
+			c.should.equal(2)
+		})
+		
+		it('Should call functions in the context of the emitter', function (done) {
+			emitter.on('test', function (d) {
+				this.should.equal(emitter)
+				done()
+			})
+			emitter.emit('test')
+		})
+		
+		it('should pass data to each handler', function (done) {
+			emitter.on('a', function (d) {
+				d.should.equal(1)
+			})		
+			emitter.on('b', function () {
+				arguments.should.deep.equal([1,2,3,4,5])
+				done()
+			})
+			emitter.emit('a', 1)
+			emitter.emit('b', 1,2,3,4,5)
+		})
+	})
+
+	describe('.off()', function () {
+		it('Should remove all topics', function () {
+			emitter.on('rad', function () {})
+			emitter.on('test', function () {})
+			emitter.off()
+			Object.keys(emitter._events).length.should.equal(0)
+		})
+		
+		// its useful occasionally
+		it('should maintain identity of the events store', function () {
+			var events = emitter._events
+			emitter.on('rad', function () {})
+			emitter.off()
+			emitter._events.should.equal(events)
+		})
+	})
+
+	describe('.off(String)', function () {
+		it('Should clear all under the given topic', function () {
+			emitter.on('test', noopA)
+			emitter.on('test', noopB)
+			emitter.off('test')
+			notSubscription('test', noopA)
+			notSubscription('test', noopB)
+		})
+	})
+
+	describe('.off(String, Function)', function () {
+		it('Should remove subscriptions which match both the topic and fn', function () {
+			emitter.on('test', noopA)
+			emitter.off('test', noopA)
+			notSubscription('test', noopA)
+		})
+
+		it('Should not remove other subscriptions', function () {
+			emitter.on('test', noopA)
+			emitter.on('test', noopB)
+			emitter.off('test', noopA)
+			notSubscription('test', noopA)
+			hasSubscription('test', noopB)
+		})
+
+		it('should remove all matching functions', function () {
+			emitter.on('test', noopA)
+			emitter.on('test', noopA)
+			emitter.off('test', noopA)
+			notSubscription('test', noopA)
+		})
+	})
+
+	describe('.once(String, Function)', function () {
+		it('should remove the subscription after once call', function () {
+			var c = 0
+			emitter.once('test', function () {
+				c++
+			})
+			emitter.emit('test')
+			emitter.emit('test')
+			c.should.equal(1)
+		})
+	})
+})
